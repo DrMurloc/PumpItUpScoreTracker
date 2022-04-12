84b390a2f309d7a31bda8e752507b533cd9bfd07
Jonathan Eccker <joneccker@gmail.com>
2022-04-11 22:15:42 -0400

    Added song and chart retrieval

---
+++ ScoreTracker/ScoreTracker.Domain/SecondaryPorts/IChartRepository.cs
@@ -0,0 +1,10 @@
+﻿using ScoreTracker.Domain.Models;
+using ScoreTracker.Domain.ValueTypes;
+
+namespace ScoreTracker.Domain.SecondaryPorts;
+
+public interface IChartRepository
+{
+    Task<IEnumerable<Name>> GetSongNames(CancellationToken cancellationToken = default);
+    Task<IEnumerable<Chart>> GetChartsForSong(Name songName, CancellationToken cancellationToken = default);
+}
