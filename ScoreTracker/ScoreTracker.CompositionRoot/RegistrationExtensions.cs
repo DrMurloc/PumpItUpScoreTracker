137de2eb08acd0b0fc54d8b80f1adcd7ea0113e2
Jonathan Eccker <joneccker@gmail.com>
2022-04-11 23:12:52 -0400

    Added base infrastructure implementation

---
+++ ScoreTracker/ScoreTracker.CompositionRoot/RegistrationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ScoreTracker.Application.Handlers;
+using ScoreTracker.Data.Persistence;
+using ScoreTracker.Data.Repositories;
+using ScoreTracker.Domain.SecondaryPorts;
+
+namespace ScoreTracker.CompositionRoot;
+
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection builder)
+    {
+        return builder.AddMediatR(typeof(RecordAttemptHandler));
+    }
+
+    public static IServiceCollection AddInfrastructure(this IServiceCollection builder)
+    {
+        foreach (var implementationType in typeof(EFChartRepository).Assembly.GetTypes()
+                )
+        foreach (var interfaceType in implementationType.GetInterfaces()
+                     .Where(i => i.Assembly == typeof(IChartRepository).Assembly))
+            builder.AddTransient(interfaceType, implementationType);
+        return builder.AddDbContext<ChartAttemptDbContext>(o => { o.UseInMemoryDatabase("ChartAttempts"); });
+    }
+}
---
+++ ScoreTracker/ScoreTracker.Data/Persistence/Entities/ChartEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScoreTracker.Data.Persistence.Entities;
+
+public sealed class ChartEntity
+{
+    [Key] public Guid Id { get; set; }
+
+    [Required] public Guid SongId { get; set; }
+
+    [Required] public int Level { get; set; }
+
+    [Required] public string Type { get; set; } = string.Empty;
+}
