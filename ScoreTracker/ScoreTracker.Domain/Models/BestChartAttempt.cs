3857a72fceda059915cb2960cdf2a23d96a14078
Jonathan Eccker <joneccker@gmail.com>
2022-04-16 22:59:45 -0400

    Added UI for viewing best attempts by chart

--- ScoreTracker/ScoreTracker.Domain/Models/BestChartAttempt.cs
+++ ScoreTracker/ScoreTracker.Domain/Models/BestChartAttempt.cs
@@ -1,6 +1,5 @@
 ﻿namespace ScoreTracker.Domain.Models;
 
 public sealed record BestChartAttempt(Chart Chart, ChartAttempt? BestAttempt)
-
 {
 }
---
+++ ScoreTracker/ScoreTracker/Dtos/BestAttemptDto.cs
@@ -0,0 +1,22 @@
+﻿using ScoreTracker.Domain.Models;
+
+namespace ScoreTracker.Web.Dtos;
+
+public class BestAttemptDto
+{
+    public string ChartType { get; set; }
+    public string SongName { get; set; }
+    public string LetterGrade { get; set; } = string.Empty;
+    public bool IsBroken { get; set; }
+
+    public static BestAttemptDto From(BestChartAttempt attempt)
+    {
+        return new BestAttemptDto
+        {
+            ChartType = attempt.Chart.Type.ToString(),
+            IsBroken = attempt.BestAttempt?.IsBroken ?? true,
+            LetterGrade = attempt.BestAttempt?.LetterGrade.ToString() ?? string.Empty,
+            SongName = attempt.Chart.SongName
+        };
+    }
+}
