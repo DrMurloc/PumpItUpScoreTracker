468d2bb94694ec6f86326faaa436580660e6d879
Jonathan Eccker <joneccker@gmail.com>
2022-04-18 22:53:00 -0400

    Made previous best fill in when you select a song

---
+++ ScoreTracker/ScoreTracker.Application/Handlers/GetBestChartAttemptHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using ScoreTracker.Application.Queries;
+using ScoreTracker.Domain.Models;
+using ScoreTracker.Domain.SecondaryPorts;
+
+namespace ScoreTracker.Application.Handlers;
+
+public sealed class GetBestChartAttemptHandler : IRequestHandler<GetBestChartAttemptQuery, BestChartAttempt>
+{
+    private readonly IChartAttemptRepository _chartAttempts;
+    private readonly IChartRepository _charts;
+    private readonly ICurrentUserAccessor _user;
+
+    public GetBestChartAttemptHandler(ICurrentUserAccessor user,
+        IChartAttemptRepository chartAttempts,
+        IChartRepository charts)
+    {
+        _user = user;
+        _chartAttempts = chartAttempts;
+        _charts = charts;
+    }
+
+    public async Task<BestChartAttempt> Handle(GetBestChartAttemptQuery request, CancellationToken cancellationToken)
+    {
+        var chart = await _charts.GetChart(request.SongName, request.ChartType, request.Level, cancellationToken);
+        var bestAttempt = await _chartAttempts.GetBestAttempt(_user.UserId, chart, cancellationToken);
+        return new BestChartAttempt(chart, bestAttempt);
+    }
+}
--- ScoreTracker/ScoreTracker.Domain/SecondaryPorts/IChartRepository.cs
+++ ScoreTracker/ScoreTracker.Domain/SecondaryPorts/IChartRepository.cs
@@ -1,4 +1,5 @@
-﻿using ScoreTracker.Domain.Models;
+﻿using ScoreTracker.Domain.Enums;
+using ScoreTracker.Domain.Models;
 using ScoreTracker.Domain.ValueTypes;
 
 namespace ScoreTracker.Domain.SecondaryPorts;
@@ -6,6 +7,10 @@ namespace ScoreTracker.Domain.SecondaryPorts;
 public interface IChartRepository
 {
     Task<IEnumerable<Name>> GetSongNames(CancellationToken cancellationToken = default);
+
+    Task<Chart> GetChart(Name songName, ChartType chartType, DifficultyLevel level,
+        CancellationToken cancellationToken = default);
+
     Task<IEnumerable<Chart>> GetChartsForSong(Name songName, CancellationToken cancellationToken = default);
 
     Task<IEnumerable<Chart>> GetChartsByDifficulty(DifficultyLevel difficultyLevel,
