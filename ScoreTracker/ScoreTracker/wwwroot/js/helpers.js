aa1f5b44609bc26907761bd27496f504b504612b
Jonathan Eccker <joneccker@gmail.com>
2022-04-24 13:21:07 -0400

    Moved upload out into its own page

--- ScoreTracker/ScoreTracker/wwwroot/js/helpers.js
+++ ScoreTracker/ScoreTracker/wwwroot/js/helpers.js
@@ -1,3 +1,25 @@
 ﻿export async function refreshLogin() {
     await fetch("/Logout/Refresh");
 }
+
+
+export async function downloadFileFromStream(fileName, contentStreamReference) {
+    console.log(contentStreamReference);
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    console.log(arrayBuffer);
+    const blob = new Blob([arrayBuffer]);
+    console.log(blob);
+        const url = URL.createObjectURL(blob);
+    
+        triggerFileDownload(fileName, url);
+
+        URL.revokeObjectURL(url);
+    }
+
+export function triggerFileDownload(fileName, url) {
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+}
