04b4fc8f5eb0c614d0dd83312f26a35fbde971b8
Jonathan Eccker <joneccker@gmail.com>
2022-04-24 14:12:28 -0400

    Fixed bug that made failed charts not downloaded

--- ScoreTracker/ScoreTracker/wwwroot/js/helpers.js
+++ ScoreTracker/ScoreTracker/wwwroot/js/helpers.js
@@ -4,17 +4,14 @@
 
 
 export async function downloadFileFromStream(fileName, contentStreamReference) {
-    console.log(contentStreamReference);
     const arrayBuffer = await contentStreamReference.arrayBuffer();
-    console.log(arrayBuffer);
     const blob = new Blob([arrayBuffer]);
-    console.log(blob);
-        const url = URL.createObjectURL(blob);
-    
-        triggerFileDownload(fileName, url);
+    const url = URL.createObjectURL(blob);
 
-        URL.revokeObjectURL(url);
-    }
+    triggerFileDownload(fileName, url);
+
+    URL.revokeObjectURL(url);
+}
 
 export function triggerFileDownload(fileName, url) {
     const anchorElement = document.createElement('a');
