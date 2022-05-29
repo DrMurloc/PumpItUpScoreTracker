d721bf0c8bd60c7fe9973ef061387f04a82e7c31
Jonathan Eccker <joneccker@gmail.com>
2022-05-29 10:04:37 -0400

    Added EF migrations for sort order

---
+++ ScoreTracker/ScoreTracker.Data/Migrations/20220529140228_Add-Song-Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScoreTracker.Data.Migrations
+{
+    public partial class AddSongOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SongOrder",
+                schema: "scores",
+                columns: table => new
+                {
+                    SongId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SongOrder", x => x.SongId);
+                    table.ForeignKey(
+                        name: "FK_SongOrder_Song_SongId",
+                        column: x => x.SongId,
+                        principalSchema: "scores",
+                        principalTable: "Song",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SongOrder",
+                schema: "scores");
+        }
+    }
+}
