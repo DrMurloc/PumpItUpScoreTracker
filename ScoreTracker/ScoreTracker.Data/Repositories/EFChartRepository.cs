2063bb1f5acdf151904692900cebceebe2e102a2
Jonathan Eccker <joneccker@gmail.com>
2023-02-12 15:38:29 -0500

    Added caching for charts and chart videos to improve performance

--- ScoreTracker/ScoreTracker.Data/Repositories/EFChartRepository.cs
+++ ScoreTracker/ScoreTracker.Data/Repositories/EFChartRepository.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using ScoreTracker.Data.Persistence;
-using ScoreTracker.Data.Persistence.Entities;
 using ScoreTracker.Domain.Enums;
 using ScoreTracker.Domain.Models;
 using ScoreTracker.Domain.SecondaryPorts;
@@ -10,33 +10,32 @@ namespace ScoreTracker.Data.Repositories;
 
 public sealed class EFChartRepository : IChartRepository
 {
+    private readonly IMemoryCache _cache;
     private readonly ChartAttemptDbContext _database;
 
-    public EFChartRepository(ChartAttemptDbContext database)
+    public EFChartRepository(ChartAttemptDbContext database, IMemoryCache cache)
     {
         _database = database;
+        _cache = cache;
     }
 
     public async Task<IEnumerable<Chart>> GetCharts(DifficultyLevel? level = null, ChartType? type = null,
         CancellationToken cancellationToken = default)
     {
-        var query = _database.Chart.AsQueryable();
+        var result = (await GetAllCharts(cancellationToken)).Values.AsEnumerable();
         if (level != null)
         {
             var levelInt = (int)level.Value;
-            query = query.Where(c => c.Level == levelInt);
+            result = result.Where(c => c.Level == levelInt);
         }
 
         if (type != null)
         {
             var typeString = type.Value.ToString();
-            query = query.Where(c => c.Type == typeString);
+            result = result.Where(c => c.Type.ToString() == typeString);
         }
 
-        return await (from c in query
-                join s in _database.Song on c.SongId equals s.Id
-                select new Chart(c.Id, new Song(s.Name, new Uri(s.ImagePath)), Enum.Parse<ChartType>(c.Type), c.Level))
-            .ToArrayAsync(cancellationToken);
+        return result;
     }
 
     public async Task<IEnumerable<Name>> GetSongNames(CancellationToken cancellationToken = default)
@@ -46,45 +45,51 @@ public sealed class EFChartRepository : IChartRepository
 
     public async Task<Chart> GetChart(Guid chartId, CancellationToken cancellationToken = default)
     {
-        return await (from c in _database.Chart
-                join s in _database.Song on c.SongId equals s.Id
-                where c.Id == chartId
-                select new Chart(c.Id, new Song(s.Name, new Uri(s.ImagePath)), Enum.Parse<ChartType>(c.Type), c.Level))
-            .SingleAsync(cancellationToken);
+        var charts = await GetAllCharts(cancellationToken);
+        return charts[chartId];
     }
 
 
     public async Task<IEnumerable<Chart>> GetChartsForSong(Name songName, CancellationToken cancellationToken = default)
     {
-        var nameString = (string)songName;
-        return await (from s in _database.Song
-                join c in _database.Chart on s.Id equals c.SongId
-                where s.Name == nameString
-                select new Chart(c.Id, new Song(s.Name, new Uri(s.ImagePath)), Enum.Parse<ChartType>(c.Type), c.Level))
-            .ToArrayAsync(cancellationToken);
+        var charts = await GetAllCharts(cancellationToken);
+        return charts.Values.Where(c => c.Song.Name == songName);
     }
 
 
     public async Task<IEnumerable<Chart>> GetCoOpCharts(CancellationToken cancellationToken = default)
     {
-        return await (from c in _database.Chart
-                join s in _database.Song on c.SongId equals s.Id
-                where c.Type == ChartType.CoOp.ToString()
-                select new Chart(c.Id, new Song(s.Name, new Uri(s.ImagePath)), Enum.Parse<ChartType>(c.Type), c.Level))
-            .ToArrayAsync(cancellationToken);
+        var charts = await GetAllCharts(cancellationToken);
+        return charts.Values.Where(c => c.Type == ChartType.CoOp);
     }
 
     public async Task<IEnumerable<ChartVideoInformation>> GetChartVideoInformation(
         IEnumerable<Guid>? chartIds = default, CancellationToken cancellationToken = default)
     {
-        IQueryable<ChartVideoEntity> query = _database.ChartVideo;
-        if (chartIds != null)
-        {
-            var chartIdArray = chartIds.ToArray();
-            query = query.Where(c => chartIdArray.Contains(c.ChartId));
-        }
+        const string key = $"{nameof(EFChartRepository)}_{nameof(GetChartVideoInformation)}";
+        var chartVideos = await _cache.GetOrCreateAsync<IDictionary<Guid, ChartVideoInformation>>(key,
+            async entry =>
+            {
+                entry.AbsoluteExpiration = DateTimeOffset.Now + TimeSpan.FromDays(14);
+                return _database.ChartVideo
+                    .Select(c => new ChartVideoInformation(c.ChartId, new Uri(c.VideoUrl), c.ChannelName))
+                    .ToDictionary(c => c.ChartId);
+            });
 
-        return await query.Select(c => new ChartVideoInformation(c.ChartId, new Uri(c.VideoUrl), c.ChannelName))
-            .ToArrayAsync(cancellationToken);
+        return chartIds != null ? chartIds.Select(id => chartVideos[id]) : chartVideos.Values;
+    }
+
+    private async Task<IDictionary<Guid, Chart>> GetAllCharts(CancellationToken cancellationToken)
+    {
+        const string key = $"{nameof(EFChartRepository)}_{nameof(GetAllCharts)}";
+        return await _cache.GetOrCreateAsync<IDictionary<Guid, Chart>>(key, async entry =>
+        {
+            entry.AbsoluteExpiration = DateTimeOffset.Now + TimeSpan.FromDays(14);
+            return await (from c in _database.Chart
+                    join s in _database.Song on c.SongId equals s.Id
+                    select new Chart(c.Id, new Song(s.Name, new Uri(s.ImagePath)), Enum.Parse<ChartType>(c.Type),
+                        c.Level))
+                .ToDictionaryAsync(c => c.Id, c => c, cancellationToken);
+        });
     }
 }
