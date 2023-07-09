7750a83274f3f49c575aeaaaec01eba42e1301cd
Jonathan Eccker <joneccker@gmail.com>
2023-07-08 23:45:43 -0400

    Added a PoC chart randomizer

---
+++ ScoreTracker/ScoreTracker.Domain/Enums/SongType.cs
@@ -0,0 +1,9 @@
+﻿namespace ScoreTracker.Domain.Enums;
+
+public enum SongType
+{
+    Arcade,
+    ShortCut,
+    FullSong,
+    Remix
+}
--- ScoreTracker/ScoreTracker.Domain/Models/Song.cs
+++ ScoreTracker/ScoreTracker.Domain/Models/Song.cs
@@ -1,7 +1,18 @@
-﻿using ScoreTracker.Domain.ValueTypes;
+﻿using ScoreTracker.Domain.Enums;
+using ScoreTracker.Domain.ValueTypes;
 
 namespace ScoreTracker.Domain.Models;
 
 public sealed record Song(Name Name, Uri ImagePath)
 {
+    public SongType Type
+    {
+        get
+        {
+            var nameString = (string)Name;
+            if (nameString.EndsWith("Full Song")) return SongType.FullSong;
+            if (nameString.EndsWith("Remix")) return SongType.Remix;
+            return nameString.EndsWith("Short Cut") ? SongType.ShortCut : SongType.Arcade;
+        }
+    }
 }
