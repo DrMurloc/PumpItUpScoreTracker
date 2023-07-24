247fe7009326ecca848d4887039cc1b2222fa61a
Jonathan Eccker <joneccker@gmail.com>
2023-07-24 16:25:18 -0700

    Fixed XX score uploading

---
+++ ScoreTracker/ScoreTracker.Application/Handlers/GetChartHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using ScoreTracker.Application.Queries;
+using ScoreTracker.Domain.Models;
+using ScoreTracker.Domain.SecondaryPorts;
+
+namespace ScoreTracker.Application.Handlers;
+
+public sealed class GetChartHandler : IRequestHandler<GetChartQuery, Chart?>
+{
+    private readonly IChartRepository _charts;
+
+    public GetChartHandler(IChartRepository charts)
+    {
+        _charts = charts;
+    }
+
+    public async Task<Chart?> Handle(GetChartQuery request, CancellationToken cancellationToken)
+    {
+        return (await _charts.GetChartsForSong(request.Mix, request.SongName, cancellationToken)).FirstOrDefault(
+            c => c.Type == request.Type && c.Level == request.Level);
+    }
+}
---
+++ ScoreTracker/ScoreTracker.Application/Queries/GetChartQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using ScoreTracker.Domain.Enums;
+using ScoreTracker.Domain.Models;
+using ScoreTracker.Domain.ValueTypes;
+
+namespace ScoreTracker.Application.Queries;
+
+public sealed record GetChartQuery(MixEnum Mix, Name SongName, DifficultyLevel Level, ChartType Type) : IRequest<Chart?>
+{
+}
