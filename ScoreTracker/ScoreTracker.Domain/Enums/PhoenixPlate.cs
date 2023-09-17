15946538acfe71f2734c863b84bc4c6bc31ff5bb
Jon E <joneccker@gmail.com>
2023-09-17 18:16:05 -0400

    Added a tournaments overview page

--- ScoreTracker/ScoreTracker.Domain/Enums/PhoenixPlate.cs
+++ ScoreTracker/ScoreTracker.Domain/Enums/PhoenixPlate.cs
@@ -5,20 +5,51 @@ namespace ScoreTracker.Domain.Enums;
 
 public enum PhoenixPlate
 {
-    [Description("Rough Game")] RoughGame,
-    [Description("Fair Game")] FairGame,
-    [Description("Talented Game")] TalentedGame,
-    [Description("Marvelous Game")] MarvelousGame,
-    [Description("Superb Game")] SuperbGame,
-    [Description("Extreme Game")] ExtremeGame,
-    [Description("Ultimate Game")] UltimateGame,
-    [Description("Perfect Game")] PerfectGame
+    [PlateShorthandAttribute("RG")] [Description("Rough Game")]
+    RoughGame,
+
+    [PlateShorthandAttribute("FG")] [Description("Fair Game")]
+    FairGame,
+
+    [PlateShorthandAttribute("TG")] [Description("Talented Game")]
+    TalentedGame,
+
+    [PlateShorthandAttribute("MG")] [Description("Marvelous Game")]
+    MarvelousGame,
+
+    [PlateShorthandAttribute("SG")] [Description("Superb Game")]
+    SuperbGame,
+
+    [PlateShorthandAttribute("EG")] [Description("Extreme Game")]
+    ExtremeGame,
+
+    [PlateShorthandAttribute("UG")] [Description("Ultimate Game")]
+    UltimateGame,
+
+    [PlateShorthandAttribute("PG")] [Description("Perfect Game")]
+    PerfectGame
+}
+
+internal sealed class PlateShorthandAttribute : Attribute
+{
+    public PlateShorthandAttribute(string shorthand)
+    {
+        Shorthand = shorthand;
+    }
+
+    public string Shorthand { get; }
 }
 
 public static class PhoenixPlateHelperMethods
 {
     private static readonly IDictionary<string, PhoenixPlate> Parser =
         Enum.GetValues<PhoenixPlate>().ToDictionary(e => e.GetName());
+
+    public static string GetShorthand(this PhoenixPlate enumValue)
+    {
+        return typeof(PhoenixPlate).GetField(enumValue.ToString())?.GetCustomAttribute<PlateShorthandAttribute>()
+            ?.Shorthand ?? enumValue.ToString();
+    }
 
     public static string GetName(this PhoenixPlate enumValue)
     {
