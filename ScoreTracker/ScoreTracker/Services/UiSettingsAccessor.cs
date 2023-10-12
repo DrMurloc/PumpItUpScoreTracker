05494802db173e887c6b642fc30aae5f80df4868
Jonathan Eccker <joneccker@gmail.com>
2023-10-11 21:08:05 -0400

    Made mix persist for non logged in users

--- ScoreTracker/ScoreTracker/Services/UiSettingsAccessor.cs
+++ ScoreTracker/ScoreTracker/Services/UiSettingsAccessor.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using ScoreTracker.Application.Commands;
 using ScoreTracker.Application.Queries;
 using ScoreTracker.Domain.Enums;
@@ -12,16 +13,25 @@ public sealed class UiSettingsAccessor : IUiSettingsAccessor
     private const string MixKey = "Universal__CurrentMix";
     private readonly ICurrentUserAccessor _currentUser;
     private readonly IMediator _mediator;
+    private readonly ProtectedLocalStorage _browserStorage;
 
-    public UiSettingsAccessor(IMediator mediator, ICurrentUserAccessor currentUser)
+    public UiSettingsAccessor(IMediator mediator, ICurrentUserAccessor currentUser,
+        ProtectedLocalStorage browserStorage)
     {
         _mediator = mediator;
         _currentUser = currentUser;
+        _browserStorage = browserStorage;
     }
 
     public async Task<MixEnum> GetSelectedMix(CancellationToken cancellationToken = default)
     {
-        if (!_currentUser.IsLoggedIn) return MixEnum.Phoenix;
+        if (!_currentUser.IsLoggedIn)
+        {
+            var browserValue = await _browserStorage.GetAsync<string>(MixKey);
+            if (browserValue.Success && Enum.TryParse<MixEnum>(browserValue.Value, out var browserMix))
+                return browserMix;
+            return MixEnum.Phoenix;
+        }
 
         var settings = await _mediator.Send(new GetUserUiSettingsQuery(), cancellationToken);
         if (!settings.ContainsKey(MixKey)) return MixEnum.Phoenix;
@@ -30,7 +40,11 @@ public sealed class UiSettingsAccessor : IUiSettingsAccessor
 
     public async Task SetSelectedMix(MixEnum mix, CancellationToken cancellationToken = default)
     {
-        if (!_currentUser.IsLoggedIn) return;
+        if (!_currentUser.IsLoggedIn)
+        {
+            await _browserStorage.SetAsync(MixKey, mix.ToString());
+            return;
+        }
 
         await _mediator.Send(new SaveUserUiSettingCommand(MixKey, mix.ToString()), cancellationToken);
     }
