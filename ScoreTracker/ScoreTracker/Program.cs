c4dbb61f9fac3dc065a4d64764894ee0e0409b53
Jonathan Eccker <jeccker@dealeron.com>
2023-10-27 10:40:28 -0400

    Added Murloc as a language

--- ScoreTracker/ScoreTracker/Program.cs
+++ ScoreTracker/ScoreTracker/Program.cs
@@ -114,8 +114,8 @@ builder.Services.AddCookiePolicy(opts =>
 
 var app = builder.Build();
 app.UseRequestLocalization(new RequestLocalizationOptions()
-    .AddSupportedCultures("en-US", "pt-BR", "ko-KR")
-    .AddSupportedUICultures("en-US", "pt-BR", "ko-KR")
+    .AddSupportedCultures("en-US", "pt-BR", "ko-KR", "en-ZW")
+    .AddSupportedUICultures("en-US", "pt-BR", "ko-KR", "en-ZW")
     .SetDefaultCulture("en-US"));
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
