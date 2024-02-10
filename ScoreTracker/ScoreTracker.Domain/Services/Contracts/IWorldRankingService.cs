34f8f58e81f4b73e024a14a8219b1e544d62866e
Jon E <joneccker@gmail.com>
2024-02-10 15:05:36 -0500

    Added ability to view top 50 charts for a player

--- ScoreTracker/ScoreTracker.Domain/Services/Contracts/IWorldRankingService.cs
+++ ScoreTracker/ScoreTracker.Domain/Services/Contracts/IWorldRankingService.cs
@@ -1,7 +1,13 @@
-﻿namespace ScoreTracker.Domain.Services.Contracts
+﻿using ScoreTracker.Domain.Models;
+using ScoreTracker.Domain.ValueTypes;
+
+namespace ScoreTracker.Domain.Services.Contracts
 {
     public interface IWorldRankingService
     {
         Task CalculateWorldRankings(CancellationToken cancellationToken);
+
+        Task<IEnumerable<RecordedPhoenixScore>> GetTop50(Name username, string type,
+            CancellationToken cancellationToken);
     }
 }
--- ScoreTracker/ScoreTracker.Domain/Services/WorldRankingService.cs
+++ ScoreTracker/ScoreTracker.Domain/Services/WorldRankingService.cs
@@ -12,11 +12,14 @@ namespace ScoreTracker.Domain.Services
     {
         private readonly IOfficialLeaderboardRepository _leaderboards;
         private readonly ILogger _logger;
+        private readonly IChartRepository _charts;
 
-        public WorldRankingService(IOfficialLeaderboardRepository leaderboards, ILogger<WorldRankingService> logger)
+        public WorldRankingService(IOfficialLeaderboardRepository leaderboards, ILogger<WorldRankingService> logger,
+            IChartRepository charts)
         {
             _leaderboards = leaderboards;
             _logger = logger;
+            _charts = charts;
         }
 
         public async Task CalculateWorldRankings(CancellationToken cancellationToken)
@@ -72,6 +75,34 @@ namespace ScoreTracker.Domain.Services
                         totalRating), cancellationToken);
                 }
             }
+        }
+
+        public async Task<IEnumerable<RecordedPhoenixScore>> GetTop50(Name username, string type,
+            CancellationToken cancellationToken)
+        {
+            var scoringConfig = new ScoringConfiguration
+            {
+                ContinuousLetterGradeScale = true
+            };
+            var result = new List<RecordedPhoenixScore>();
+            foreach (var record in (await _leaderboards.GetOfficialLeaderboardStatuses(username, cancellationToken))
+                     .Where(l => l.OfficialLeaderboardType == "Chart" && !l.LeaderboardName.Contains("CoOp"))
+                     .Select(l => (l.Score, DifficultyLevel.ParseShortHand(l.LeaderboardName.Split(" ")[^1]),
+                         l.LeaderboardName))
+                     .Where(l => type == "All" || (type == "Singles" && l.Item2.chartType == ChartType.Single) ||
+                                 (type == "Doubles" && l.Item2.chartType == ChartType.Double))
+                     .OrderByDescending(l => scoringConfig.GetScore(l.Item2.level, l.Score))
+                     .Take(50))
+            {
+                var songName = string.Join(" ", record.LeaderboardName.Split(" ").Reverse().Skip(1).Reverse());
+                var charts = await _charts.GetChartsForSong(MixEnum.Phoenix, songName, cancellationToken);
+                var chart = charts
+                    .FirstOrDefault(c => c.Type == record.Item2.chartType && c.Level == record.Item2.level);
+                if (chart != null)
+                    result.Add(new RecordedPhoenixScore(chart.Id, record.Score, null, false, DateTimeOffset.Now));
+            }
+
+            return result;
         }
     }
 }
