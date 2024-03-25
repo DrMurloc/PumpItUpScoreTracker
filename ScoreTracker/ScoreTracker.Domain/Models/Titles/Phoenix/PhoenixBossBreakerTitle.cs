89e7aca667c5a72e8aceb19895ba4411bb917669
Jon E <joneccker@gmail.com>
2024-03-25 11:56:13 -0400

    Added all titles

---
+++ ScoreTracker/ScoreTracker.Domain/Models/Titles/Phoenix/PhoenixBossBreakerTitle.cs
@@ -0,0 +1,31 @@
+﻿using ScoreTracker.Domain.Enums;
+using ScoreTracker.Domain.ValueTypes;
+
+namespace ScoreTracker.Domain.Models.Titles.Phoenix
+{
+    public sealed class PhoenixBossBreakerTitle : PhoenixTitle
+    {
+        private readonly Name _songName;
+        private readonly ChartType _chartType;
+        private readonly DifficultyLevel _level;
+
+        public PhoenixBossBreakerTitle(Name mix, Name songName, ChartType chartType, DifficultyLevel level,
+            bool includeChartType = true) : base(
+            $"[{mix}] {(includeChartType ? chartType + " " : "")}Boss breaker",
+            $"Pass {songName} {chartType.GetShortHand()}{level}", "Boss Breaker",
+            1)
+        {
+            _songName = songName;
+            _chartType = chartType;
+            _level = level;
+        }
+
+        public override double CompletionProgress(Chart chart, RecordedPhoenixScore attempt)
+        {
+            if (chart.Song.Name == _songName && _chartType == chart.Type && _level == chart.Level &&
+                !attempt.IsBroken) return 1;
+
+            return 0;
+        }
+    }
+}
---
+++ ScoreTracker/ScoreTracker.Domain/Models/Titles/Phoenix/PhoenixSkillTitle.cs
@@ -0,0 +1,31 @@
+﻿using ScoreTracker.Domain.Enums;
+using ScoreTracker.Domain.ValueTypes;
+
+namespace ScoreTracker.Domain.Models.Titles.Phoenix
+{
+    public sealed class PhoenixSkillTitle : PhoenixTitle
+    {
+        private readonly Name _songName;
+        private readonly ChartType _chartType;
+        private readonly DifficultyLevel _level;
+
+        public PhoenixSkillTitle(Name skill, int skillLevel, Name songName, ChartType chartType, DifficultyLevel level,
+            PhoenixLetterGrade letterRequirement = PhoenixLetterGrade.SSS) : base(
+            $"[{skill}] Lv.{skillLevel}",
+            $"Get {letterRequirement.GetName()} on {songName} {chartType.GetShortHand()}{level}", "Skill",
+            990000)
+        {
+            _songName = songName;
+            _chartType = chartType;
+            _level = level;
+        }
+
+        public override double CompletionProgress(Chart chart, RecordedPhoenixScore attempt)
+        {
+            if (chart.Song.Name == _songName && _chartType == chart.Type && _level == chart.Level &&
+                attempt.Score != null) return attempt.Score.Value;
+
+            return 0;
+        }
+    }
+}
