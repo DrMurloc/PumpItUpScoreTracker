a71b1fccd10776512f9905e1d03521ae76c1b7d7
Jon E <joneccker@gmail.com>
2024-04-16 18:16:48 -0400

    Qualifiers page update

--- ScoreTracker/ScoreTracker.Domain/Models/UserQualifiers.cs
+++ ScoreTracker/ScoreTracker.Domain/Models/UserQualifiers.cs
@@ -1,97 +1,93 @@
 ﻿using ScoreTracker.Domain.Records;
 using ScoreTracker.Domain.ValueTypes;
 
-namespace ScoreTracker.Domain.Models
+namespace ScoreTracker.Domain.Models;
+
+public sealed class UserQualifiers
 {
-    public sealed class UserQualifiers
+    private readonly ScoringConfiguration _scoreConfig = new()
     {
-        public QualifiersConfiguration Configuration { get; }
+        ContinuousLetterGradeScale = true
+    };
 
-        public bool IsApproved { get; private set; }
-        public Name UserName { get; set; }
+    public UserQualifiers(QualifiersConfiguration config, bool isApproved, Name userName,
+        IDictionary<Guid, Submission> submissions)
+    {
+        Configuration = config;
+        IsApproved = isApproved;
+        UserName = userName;
+        Submissions = submissions;
+    }
 
-        public IDictionary<Guid, Submission> Submissions { get; }
+    public QualifiersConfiguration Configuration { get; }
 
-        public IEnumerable<(Chart, PhoenixScore)> BestCharts()
+    public bool IsApproved { get; private set; }
+    public Name UserName { get; set; }
+
+    public IDictionary<Guid, Submission> Submissions { get; }
+
+    public IEnumerable<(Chart Chart, PhoenixScore Score, double Rating)> BestCharts()
+    {
+        return Configuration.Charts.Where(c => Submissions.ContainsKey(c.Id)).OrderByDescending(c => Rating(c.Id))
+            .Select(c => (c, Submissions[c.Id].Score, Rating(c.Id)))
+            .Take(Configuration.PlayCount).ToArray();
+    }
+
+    public double Rating(DifficultyLevel level, PhoenixScore score)
+    {
+        if (Configuration.ScoringType == "Fungpapi") return level + (score - 965000.0) / 17500.0;
+        return _scoreConfig.GetScore(level, score);
+    }
+
+    public double Rating(Guid chartId)
+    {
+        if (!Submissions.ContainsKey(chartId)) return 0;
+
+        var difficulty = Configuration.Charts.First(c => c.Id == chartId).Level;
+        return Rating(difficulty, Submissions[chartId].Score);
+    }
+
+    public double CalculateScore()
+    {
+        return BestCharts().Sum(c => Rating(c.Item1.Id));
+    }
+
+    public void Approve()
+    {
+        IsApproved = true;
+    }
+
+    public bool AddXXScore(Guid chartId, StepCount perfects, StepCount greats, StepCount goods, StepCount bads,
+        StepCount misses, StepCount maxCombo,
+        Uri uri)
+    {
+        var offset = Configuration.NoteCountAdjustments.TryGetValue(chartId, out var adjustment)
+            ? adjustment
+            : 0;
+        perfects += offset;
+        maxCombo += offset;
+        var scoreScreen = new ScoreScreen(perfects, greats, goods, bads, misses, maxCombo);
+        return AddPhoenixScore(chartId, scoreScreen.CalculatePhoenixScore, uri);
+    }
+
+    public bool AddPhoenixScore(Guid chartId, PhoenixScore score, Uri uri)
+    {
+        if (Submissions.ContainsKey(chartId) && Submissions[chartId].Score > score) return false;
+
+        Submissions[chartId] = new Submission
         {
-            return Configuration.Charts.Where(c => Submissions.ContainsKey(c.Id)).OrderByDescending(c => Rating(c.Id))
-                .Select(c => (c, Submissions[c.Id].Score))
-                .Take(Configuration.PlayCount).ToArray();
-        }
-
-        private readonly ScoringConfiguration _scoreConfig = new()
-        {
-            ContinuousLetterGradeScale = true
+            ChartId = chartId,
+            PhotoUrl = uri,
+            Score = score
         };
+        IsApproved = false;
+        return true;
+    }
 
-        public double Rating(DifficultyLevel level, PhoenixScore score)
-        {
-            if (Configuration.ScoringType == "Fungpapi") return level + (score - 965000.0) / 17500.0;
-            return _scoreConfig.GetScore(level, score);
-        }
-
-        public double Rating(Guid chartId)
-        {
-            if (!Submissions.ContainsKey(chartId))
-            {
-                return 0;
-            }
-
-            var difficulty = Configuration.Charts.First(c => c.Id == chartId).Level;
-            return Rating(difficulty, Submissions[chartId].Score);
-        }
-
-        public double CalculateScore()
-        {
-            return BestCharts().Sum(c => Rating(c.Item1.Id));
-        }
-
-        public void Approve()
-        {
-            IsApproved = true;
-        }
-
-        public UserQualifiers(QualifiersConfiguration config, bool isApproved, Name userName,
-            IDictionary<Guid, Submission> submissions)
-        {
-            Configuration = config;
-            IsApproved = isApproved;
-            UserName = userName;
-            Submissions = submissions;
-        }
-
-        public bool AddXXScore(Guid chartId, StepCount perfects, StepCount greats, StepCount goods, StepCount bads,
-            StepCount misses, StepCount maxCombo,
-            Uri uri)
-        {
-            var offset = Configuration.NoteCountAdjustments.TryGetValue(chartId, out var adjustment)
-                ? adjustment
-                : 0;
-            perfects += offset;
-            maxCombo += offset;
-            var scoreScreen = new ScoreScreen(perfects, greats, goods, bads, misses, maxCombo);
-            return AddPhoenixScore(chartId, scoreScreen.CalculatePhoenixScore, uri);
-        }
-
-        public bool AddPhoenixScore(Guid chartId, PhoenixScore score, Uri uri)
-        {
-            if (Submissions.ContainsKey(chartId) && Submissions[chartId].Score > score) return false;
-
-            Submissions[chartId] = new Submission
-            {
-                ChartId = chartId,
-                PhotoUrl = uri,
-                Score = score
-            };
-            IsApproved = false;
-            return true;
-        }
-
-        public sealed class Submission
-        {
-            public Guid ChartId { get; set; }
-            public PhoenixScore Score { get; set; }
-            public Uri PhotoUrl { get; set; }
-        }
+    public sealed class Submission
+    {
+        public Guid ChartId { get; set; }
+        public PhoenixScore Score { get; set; }
+        public Uri PhotoUrl { get; set; }
     }
 }
