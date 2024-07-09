7731129a25289a5925edd58e0884a2adf9333a2e
Jon E <joneccker@gmail.com>
2024-07-09 19:51:50 -0400

    Points are now calculated and shown as they are typed in

--- ScoreTracker/ScoreTracker.Application/Handlers/MatchSaga.cs
+++ ScoreTracker/ScoreTracker.Application/Handlers/MatchSaga.cs
@@ -219,36 +219,7 @@ public sealed class MatchSaga : IRequestHandler<GetMatchQuery, MatchView>,
     public async Task Handle(ResolveMatchCommand request, CancellationToken cancellationToken)
     {
         var match = await _matchRepository.GetMatch(request.TournamentId, request.MatchName, cancellationToken);
-        var scoring = match.Players.Length == 2
-            ? new[] { 1, 0 }
-            : match.PointsPerPlace ??
-              match.Players.Select((p, i) => i + 1).OrderByDescending(i => i).ToArray();
-
-        for (var chartIndex = 0; chartIndex < match.ActiveCharts.Length; chartIndex++)
-        {
-            var pointIndex = 0;
-            foreach (var scoreGroup in match.Players.GroupBy(p => (int)match.Scores[p][chartIndex])
-                         .OrderByDescending(g => g.Key))
-            {
-                var points = scoreGroup.Key == 0 ? 0 : scoring[pointIndex];
-                foreach (var player in scoreGroup) match.Points[player][chartIndex] = points;
-
-                pointIndex += scoreGroup.Count();
-            }
-        }
-
-        var currentPosition = 0;
-
-        foreach (var tie in match.Players.GroupBy(p => match.Points[p].Sum())
-                     .OrderByDescending(g => g.Key))
-        foreach (var tieBreakerResult in tie.OrderByDescending(name => match.Scores[name].Sum(s => s)))
-            match.FinalPlaces[currentPosition++] = tieBreakerResult;
-
-        var newMatchState = match with
-        {
-            State = MatchState.Finalizing,
-            Machine = ""
-        };
+        var newMatchState = match.CalculatePoints();
         await _matchRepository.SaveMatch(request.TournamentId, newMatchState, cancellationToken);
 
         await _mediator.Publish(new MatchUpdatedEvent(request.TournamentId, newMatchState), cancellationToken);
--- ScoreTracker/ScoreTracker.Domain/Views/MatchView.cs
+++ ScoreTracker/ScoreTracker.Domain/Views/MatchView.cs
@@ -11,4 +11,34 @@ public sealed record MatchView(Name MatchName, Name PhaseName, int MatchOrder, i
     IDictionary<string, int[]> Points, Name[] FinalPlaces, int Round = 1, string Machine = "",
     int[]? PointsPerPlace = null)
 {
+    public MatchView CalculatePoints()
+    {
+        var match = this with { };
+
+        var scoring = match.Players.Length == 2
+            ? new[] { 1, 0 }
+            : match.PointsPerPlace ??
+              match.Players.Select((p, i) => i + 1).OrderByDescending(i => i).ToArray();
+
+        for (var chartIndex = 0; chartIndex < match.ActiveCharts.Length; chartIndex++)
+        {
+            var pointIndex = 0;
+            foreach (var scoreGroup in match.Players.GroupBy(p => (int)match.Scores[p][chartIndex])
+                         .OrderByDescending(g => g.Key))
+            {
+                var points = scoreGroup.Key == 0 ? 0 : scoring[pointIndex];
+                foreach (var player in scoreGroup) match.Points[player][chartIndex] = points;
+
+                pointIndex += scoreGroup.Count();
+            }
+        }
+
+        var currentPosition = 0;
+
+        foreach (var tie in match.Players.GroupBy(p => match.Points[p].Sum())
+                     .OrderByDescending(g => g.Key))
+        foreach (var tieBreakerResult in tie.OrderByDescending(name => match.Scores[name].Sum(s => s)))
+            match.FinalPlaces[currentPosition++] = tieBreakerResult;
+        return match;
+    }
 }
