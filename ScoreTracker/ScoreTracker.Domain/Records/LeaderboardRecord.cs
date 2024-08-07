27e2d888333aae4a5cb7eca9276fa0bad83cd57e
Jon E <joneccker@gmail.com>
2024-08-07 17:02:49 -0400

    Added Average PPS for stamina tournaments

--- ScoreTracker/ScoreTracker.Domain/Records/LeaderboardRecord.cs
+++ ScoreTracker/ScoreTracker.Domain/Records/LeaderboardRecord.cs
@@ -13,6 +13,10 @@ namespace ScoreTracker.Domain.Records
         public PhoenixScore AverageScore => (int)Session.Entries.Average(e => e.Score);
         public int HighestLevel => Session.Entries.Max(e => (int)e.Chart.Level);
         public int LowestLevel => Session.Entries.Min(e => (int)e.Chart.Level);
+
+        public double AveragePps => Session.Entries.Sum(e => e.SessionScore) /
+                                    Session.Entries.Sum(e => e.Chart.Song.Duration.TotalSeconds);
+
         public PhoenixPlate AveragePlate => (PhoenixPlate)(int)Math.Floor(Session.Entries.Average(e => (int)e.Plate));
     }
 }
